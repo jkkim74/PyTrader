86bb8535e8cd063e619e68ac0803bd09c7c94159
jkkim74 <jack68@naver.com>
2019-02-13 16:01:22 +0900

    pytrade 개발

--- Kiwoom.py
+++ Kiwoom.py
@@ -14,6 +14,39 @@ class Kiwoom(QAxWidget):
         self._create_kiwoom_instance()
         self._set_signal_slots()
 
+    @staticmethod
+    def change_format(data):
+        strip_data = data.lstrip('-0')
+        if strip_data == '':
+            strip_data = '0'
+        try:
+            format_data = format(int(strip_data), ',d')
+        except ValueError:
+            format_data = format(float(strip_data))
+        if data.startswith('-'):
+            format_data = '-' + format_data
+
+        return format_data
+
+    @staticmethod
+    def change_format2(data):
+        strip_data = data.lstrip('-0')
+
+        if strip_data == '':
+            strip_data = '0'
+
+        if strip_data.startswith('.'):
+            strip_data = '0' + strip_data
+
+        if data.startswith('-'):
+            strip_data = '-' + strip_data
+
+        return strip_data
+
+    def get_server_gubun(self):
+        ret = self.dynamicCall("KOA_Functions(QString, QString)", "GetServerGubun", "")
+        return ret
+
     def _create_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
@@ -73,6 +106,10 @@ class Kiwoom(QAxWidget):
 
         if rqname == "opt10081_req":
             self._opt10081(rqname, trcode)
+        elif rqname == "opw00001_req":
+            self._opw00001(rqname, trcode)
+        elif rqname == "opw00018_req":
+            self._opw00018(rqname, trcode)
 
         try:
             self.tr_event_loop.exit()
@@ -97,6 +134,50 @@ class Kiwoom(QAxWidget):
             self.ohlcv['close'].append(int(close))
             self.ohlcv['volume'].append(int(volume))
 
+    def _opw00001(self, rqname, trcode):
+        self.d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
+        self.d2_deposit = Kiwoom.change_format(self.d2_deposit)
+
+    def reset_opw00018_output(self):
+        self.opw00018_output = {'single': [], 'multi': []}
+
+    def _opw00018(self, rqname, trcode):
+        total_purchase_price = self._comm_get_data(trcode, "", rqname, 0, "총매입금액")
+        total_eval_price = self._comm_get_data(trcode, "", rqname, 0, "총평가금액")
+        total_eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, 0, "총평가손익금액")
+        total_earning_rate = self._comm_get_data(trcode, '', rqname, 0, "총수익률(%)")
+        estimated_deposit = self._comm_get_data(trcode, '', rqname, 0, "추정예탁자산")
+
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_purchase_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_eval_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_eval_profit_loss_price))
+
+        # _opw00018 메서드에서 모의투자일 때는 총수익률(%)의 값을 100으로 나눈 후 출력 되도록 다음과 같이 코드 수정.
+        # if self.get_server_gubun():
+        #     total_earning_rate = float(total_earning_rate) / 100
+        #     total_earning_rate = str(total_earning_rate)
+
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_earning_rate))
+        self.opw00018_output['single'].append(Kiwoom.change_format(estimated_deposit))
+
+        # multi data
+        rows = self._get_repeat_cnt(trcode, rqname)
+        for i in range(rows):
+            name = self._comm_get_data(trcode, "", rqname, i, "종목명")
+            quantity = self._comm_get_data(trcode, "", rqname, i, "보유수량")
+            purchase_price = self._comm_get_data(trcode, "", rqname, i, "매입가")
+            current_price = self._comm_get_data(trcode, "", rqname, i, "현재가")
+            eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, i, "평가손익")
+            earning_rate = self._comm_get_data(trcode, "", rqname, i, "수익률(%)")
+
+            quantity = Kiwoom.change_format(quantity)
+            purchase_price = Kiwoom.change_format(purchase_price)
+            current_price = Kiwoom.change_format(current_price)
+            eval_profit_loss_price = Kiwoom.change_format(eval_profit_loss_price)
+            earning_rate = Kiwoom.change_format2(earning_rate)
+
+            self.opw00018_output['multi'].append([name, quantity, purchase_price, current_price, eval_profit_loss_price, earning_rate])
+
     def get_chejan_data(self, fid):
         ret = self.dynamicCall("GetChejanData(int)", fid)
         return ret
@@ -116,3 +197,14 @@ class Kiwoom(QAxWidget):
         ret = self.dynamicCall("GetLoginInfo(QString)", tag)
         return ret
 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    kiwoom = Kiwoom()
+    kiwoom.comm_connect()
+
+    account_number = kiwoom.get_login_info("ACCNO")
+    account_number = account_number.split(';')[0]
+
+    kiwoom.set_input_value("계좌번호", account_number)
+    kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
--- pytrader.py
+++ pytrader.py
@@ -11,22 +11,49 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
 
+        self.trade_stocks_done = False # 자동 trading False이면 작동
+
         self.kiwoom = Kiwoom()
         self.kiwoom.comm_connect()
 
+        # Timer1
         self.timer = QTimer(self)
         self.timer.start(1000)
-        self.timer.timeout.connect(self.timeout)
-        self.lineEdit.textChanged.connect(self.code_changed)
-        self.pushButton.clicked.connect(self.send_order)
 
+        # Timer2
+        self.timer2 = QTimer(self)
+        self.timer2.start(1000 * 10)
+
+        # Timer2
+        self.timer3 = QTimer(self)
+        self.timer3.start(1000 * 10)
+
+
+        # 계좌정보 넣어줌
         accouns_num = int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
         accounts = self.kiwoom.get_login_info("ACCNO")
         accounts_list = accounts.split(';')[0:accouns_num]
         self.comboBox.addItems(accounts_list)
 
+        # 매도/매수 리스트 조회
+        self.load_buy_sell_list()
+
+        # 이벤트 정보 연결
+        self.timer.timeout.connect(self.timeout)
+        self.timer2.timeout.connect(self.timeout2)
+        self.timer3.timeout.connect(self.timeout3)
+        self.lineEdit.textChanged.connect(self.code_changed)
+        self.pushButton.clicked.connect(self.send_order)
+        self.pushButton_2.clicked.connect(self.check_balance) # pushButton_2 라는 객체가 클릭될 때 check_balance라는 메서드가 호출
+
     def timeout(self):
+        market_start_time = QTime(9, 0, 0)
         current_time = QTime.currentTime()
+
+        if current_time > market_start_time and self.trade_stocks_done is False:
+            self.trade_stocks()
+            self.trade_stocks_done = True
+
         text_time = current_time.toString("hh:mm:ss")
         time_msg = "현재시간: " + text_time
 
@@ -37,6 +64,14 @@ class MyWindow(QMainWindow, form_class):
             state_msg = "서버 미 연결 중"
 
         self.statusbar.showMessage(state_msg + " | " + time_msg)
+
+    def timeout2(self):
+        if self.checkBox.isChecked():
+            self.check_balance()
+
+    def timeout3(self):
+        if self.checkBox_2.isChecked():
+            self.load_buy_sell_list()
 
     def code_changed(self):
         code = self.lineEdit.text()
@@ -56,6 +91,143 @@ class MyWindow(QMainWindow, form_class):
 
         self.kiwoom.send_order("send_order_req", "0101", account, order_type_lookup[order_type], code, num, price,
                                hoga_lookup[hoga], "")
+
+    def check_balance(self):
+        self.kiwoom.reset_opw00018_output()
+        account_number = self.kiwoom.get_login_info("ACCNO")
+        account_number = account_number.split(';')[0]# 첫번째 계좌번호 호출
+
+        self.kiwoom.set_input_value("계좌번호", account_number)
+        self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "2000")
+
+        while self.kiwoom.remained_data:
+            time.sleep(2)
+            self.kiwoom.set_input_value("계좌번호", account_number)
+            self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 2, "2000")
+
+        # opw00001
+        self.kiwoom.set_input_value("계좌번호", account_number)
+        self.kiwoom.comm_rq_data("opw00001_req", "opw00001", 0, "2000")
+
+        # balance
+        item = QTableWidgetItem(self.kiwoom.d2_deposit)
+        item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+        self.tableWidget.setItem(0, 0, item)
+
+        # 총매입, 총평가, 총손익, 총수익률(%), 추정자산을 QTableWidget의 칼럼에 추가하는 코드.
+        # 데이터는 self.kiwoom.opw00018_output['single']을 통해 얻어올 수 있음.
+        for i in range(1, 6):
+            item = QTableWidgetItem(self.kiwoom.opw00018_output['single'][i - 1])
+            item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+            self.tableWidget.setItem(0, i, item)
+
+        # resizeRowsToContents 메서드를 호출해서 아이템의 크기에 맞춰 행의 높이를 조절
+        self.tableWidget.resizeRowsToContents()
+
+        # Item list
+        item_count = len(self.kiwoom.opw00018_output['multi'])
+        self.tableWidget_2.setRowCount(item_count)
+
+        # 한 종목에 대한 종목명, 보유량, 매입가, 현재가, 평가손익, 수익률(%)은 출력
+        for j in range(item_count):
+            row = self.kiwoom.opw00018_output['multi'][j]
+            for i in range(len(row)):
+                item = QTableWidgetItem(row[i])
+                item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+                self.tableWidget_2.setItem(j, i, item)
+
+        # resizeRowsToContents 메서드를 호출해서 아이템의 크기에 맞춰 행의 높이를 조절
+        self.tableWidget.resizeRowsToContents()
+
+    def load_buy_sell_list(self):
+        f = open("buy_list.txt", 'rt', encoding='UTF-8')
+        buy_list = f.readlines()
+        f.close()
+
+        f = open("sell_list.txt", 'rt', encoding='UTF-8')
+        sell_list = f.readlines()
+        f.close()
+
+        row_count = len(buy_list) + len(sell_list)
+        self.tableWidget_3.setRowCount(row_count)
+
+        # buy list
+        for j in range(len(buy_list)):
+            row_data = buy_list[j]
+            split_row_data = row_data.split(';')
+            split_row_data[1] = self.kiwoom.get_master_code_name(split_row_data[1].rsplit())
+
+            for i in range(len(split_row_data)):
+                item = QTableWidgetItem(split_row_data[i].rstrip())
+                item.setTextAlignment(Qt.AlignVCenter | Qt.AlignCenter)
+                self.tableWidget_3.setItem(j, i, item)
+
+        # sell list
+        for j in range(len(sell_list)):
+            row_data = sell_list[j]
+            split_row_data = row_data.split(';')
+            split_row_data[1] = self.kiwoom.get_master_code_name(split_row_data[1].rstrip())
+
+            for i in range(len(split_row_data)):
+                item = QTableWidgetItem(split_row_data[i].rstrip())
+                item.setTextAlignment(Qt.AlignVCenter | Qt.AlignCenter)
+                self.tableWidget_3.setItem(len(buy_list) + j, i, item)
+
+        self.tableWidget_3.resizeRowsToContents()
+
+    def trade_stocks(self):
+        hoga_lookup = {'지정가': "00", '시장가': "03"}
+        f = open("buy_list.txt", 'rt', encoding='UTF-8')
+        buy_list = f.readlines()
+        f.close()
+
+        f = open("sell_list.txt", 'rt', encoding='UTF-8')
+        sell_list = f.readlines()
+        f.close()
+
+        account = self.comboBox.currentText()
+
+        # buy list
+        for row_data in buy_list:
+            split_row_data = row_data.split(';')
+            hoga = split_row_data[2]
+            code = split_row_data[1]
+            num = split_row_data[3]
+            price = split_row_data[4]
+
+            if split_row_data[-1].rstrip() == '매수전':
+                self.kiwoom.send_order("send_order_req", "0101", account, 1, code, num, price, hoga_lookup[hoga], "") # 1: 매수, 2: 매도
+
+        # sell list
+        for row_data in sell_list:
+            split_row_data = row_data.split(';')
+            hoga = split_row_data[2]
+            code = split_row_data[1]
+            num = split_row_data[3]
+            price = split_row_data[4]
+
+            if split_row_data[-1].rstrip() == '매도전':
+                self.kiwoom.send_order("send_order_req", "0101", account, 2, code, num, price, hoga_lookup[hoga], "") # 1: 매수, 2: 매도
+        # buy list
+        for i, row_data in enumerate(buy_list):
+            buy_list[i] = buy_list[i].replace("매수전", "주문완료")
+
+        # file update
+        f = open("buy_list.txt", 'wt', encoding='UTF-8')
+        for row_data in buy_list:
+            f.write(row_data)
+        f.close()
+
+        # sell list
+        for i, row_data in enumerate(sell_list):
+            sell_list[i] = sell_list[i].replace("매도전", "주문완료")
+
+        # file update
+        f = open("sell_list.txt", 'wt', encoding='UTF-8')
+        for row_data in sell_list:
+            f.write(row_data)
+        f.close()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
